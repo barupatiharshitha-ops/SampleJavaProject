b786edded723cc21b7979a9f0990e303c1a62d14
BH <bh@gmail.com>
2025-10-15 09:47:19 -0500

    Added greeting message in feature branch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,1 +1,9 @@
-"public class HelloWorld { public static void main(String[] args){ System.out.println(\"Hello Git!\"); }}" 
+public class HelloWorld 
+{ 
+    public static void main(String[] args)
+    { 
+        System.out.println("Hello Git!"); 
+        System.out.println("Welcome to feature branch!");
+    }
+}
+
